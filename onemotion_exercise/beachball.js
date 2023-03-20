e47a02cc16fd5cf1263584b8407ab4e3c9bc873a
TheFred94 <frederikrlarsen@gmail.com>
2023-03-20 13:30:17 +0100

    ball punctures at end

--- onemotion_exercise/beachball.js
+++ onemotion_exercise/beachball.js
@@ -1,4 +1,6 @@
 "use strict";
-import { animate } from "https://cdn.skypack.dev/motion";
+import { animate, stagger } from "https://cdn.skypack.dev/motion";
 
-animate(".ball", { x: 400, rotate: 200 }, { duration: 2 });
+animate(".ball", { x: 400, rotate: 200 }, { duration: 2 }).finished.then(() => {
+  animate(".ball", { scale: 0 });
+});
