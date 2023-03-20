d94ab27a5974dddf68653c397b97175699f77b04
TheFred94 <frederikrlarsen@gmail.com>
2023-03-20 13:09:22 +0100

    stagger animation

--- animations_motion_one/motionone.js
+++ animations_motion_one/motionone.js
@@ -1,3 +1,5 @@
 "use strict";
 
-import { animate } from "https://cdn.skypack.dev/motion";
+import { animate, stagger } from "https://cdn.skypack.dev/motion";
+
+animate(".box", { transform: "rotate(45deg)" }, { duration: 1, delay: stagger(0.2) });
