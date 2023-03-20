940f2ff2af899305c1862e0fd3c36645de7a6912
TheFred94 <frederikrlarsen@gmail.com>
2023-03-20 14:09:48 +0100

    bounce baby

--- onemotion_exercise/multiballs.js
+++ onemotion_exercise/multiballs.js
@@ -1,1 +1,9 @@
 "use strict";
+import { animate, stagger } from "https://cdn.skypack.dev/motion";
+
+animate(".ball", { rotate: 360 }, { duration: 1.5, repeat: Infinity, easing: "linear" });
+
+animate(".ball", { y: -400 }, { duration: 1.5, delay: stagger(0.2), easing: "ease-out", direction: "alternate", repeat: "Infinity" });
+
+// animate(".ball", { x: 400, rotate: 200 }, { duration: 2, delay: stagger(0.2) }).finished.then(() => {
+// });
