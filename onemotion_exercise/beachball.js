0bcb560388d1c37f7b01d7eb83886dea53bd3488
TheFred94 <frederikrlarsen@gmail.com>
2023-03-20 14:43:43 +0100

    get outta here

--- onemotion_exercise/beachball.js
+++ onemotion_exercise/beachball.js
@@ -1,6 +1,10 @@
 "use strict";
-import { animate, stagger } from "https://cdn.skypack.dev/motion";
+import { animate, timeline } from "https://cdn.skypack.dev/motion";
 
-animate(".ball", { x: 400, rotate: 200 }, { duration: 2 }).finished.then(() => {
-  animate(".ball", { scale: 0 }, { duration: 1 });
-});
+const sequence = [
+  [".ball", { x: 400, rotate: 200 }, { duration: 2 }],
+  [".ball", { scale: 0 }, { duration: 1 }],
+  [".ball", { scale: 1, x: 1300, y: -1000 }, { duration: 2 }],
+];
+
+timeline(sequence);
