3165b593815e4876a117556d2f395d997f63cd55
TheFred94 <frederikrlarsen@gmail.com>
2023-03-23 10:22:18 +0100

    Sum changes

--- onemotion_exercise/beachball.js
+++ onemotion_exercise/beachball.js
@@ -1,9 +1,9 @@
 "use strict";
-import { animate, timeline } from "https://cdn.skypack.dev/motion";
+import { timeline } from "https://cdn.skypack.dev/motion";
 
 const sequence = [
-  [".ball", { x: 400, rotate: 200 }, { duration: 1 }, { easing: "ease-in" }],
-  [".ball", { rotate: 1000, scale: 0, x: 1300, y: -1000 }, { duration: 3 }, { easing: "ease-in" }],
+  [".ball", { x: 400, rotate: 200 }, { duration: 0.5 }, { easing: "ease" }],
+  [".ball", { rotate: 1000, scale: 0, x: 1300, y: -1000 }, { duration: 1 }, { easing: "ease" }],
 ];
 
-timeline(sequence);
+timeline(sequence, { repeat: "Infinity" });
