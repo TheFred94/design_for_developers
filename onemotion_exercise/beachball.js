440e4809ebc684088e36315f8599857eda64834b
TheFred94 <frederikrlarsen@gmail.com>
2023-03-25 14:13:20 +0100

    changes

--- onemotion_exercise/beachball.js
+++ onemotion_exercise/beachball.js
@@ -3,6 +3,7 @@ import { timeline } from "https://cdn.skypack.dev/motion";
 
 const sequence = [
   [".ball", { x: 400, rotate: 200 }, { duration: 0.5 }, { easing: "ease" }],
+
   [".ball", { rotate: 1000, scale: 0, x: 1300, y: -1000 }, { duration: 1 }, { easing: "ease" }],
 ];
 
